e6c9c818eab79f303fd48fa05154e12dc725469b
VinBdev <vbpod77@gmail.com>
2022-04-16 09:50:38 +0000

    Added sales dashboard

--- app.py
+++ app.py
@@ -47,6 +47,7 @@ def register():
         # put new user into session cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful")
+        return redirect(url_for("dashboard", username=session["user"]))
     return render_template("register.html") 
 
 
@@ -62,7 +63,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "dashboard", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -74,6 +78,14 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/dashboard/<username>", methods=["GET", "POST"])
+def dashboard(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("dashboard.html", username=username)
 
 
 if __name__ == "__main__":
